2ccd9bdf285ff8c72d85d6898eeb7d7bd34916ac
mohdmaazzaidi <mohdmaazzaidi@gmail.com>
2026-08-01 17:57:21 +0530

    Data_types

--- test.js
+++ test.js
@@ -19,9 +19,49 @@
 // console.log(ab);
 
 let a = "2"+(1+5);
-console.log(a);
+// console.log(a);
 
 let b = 3 + 6 + "23";
+// console.log(b);
 
-console.log(b);
+
+
+// Premitive data types
+
+let num = 123;
+let str = "Maaz";
+let bool = true;
+let temp = null;
+let undefineNum;
+let bigNum = 34533221113322334432435n;
+let id = Symbol(123);
+
+// console.log(typeof num);
+// console.log(typeof str);
+// console.log(typeof bool);
+// console.log(typeof temp);
+// console.log(typeof undefineNum);
+// console.log(typeof bigNum);
+// console.log(typeof id);
+
+
+// Non-Premitive data types are (Arrar,function,objects)
+
+let arr = [1,2,3,3,4.4]
+
+let obj = {
+    Name: 'Maaz',
+    age: 23,
+};
+
+let printMsg = function (){
+    console.log("Hello world");
+}
+
+console.log(typeof arr);
+console.log(typeof obj);
+console.log(typeof printMsg);
+
+
+
 
