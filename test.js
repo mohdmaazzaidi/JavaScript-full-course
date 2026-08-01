963c1659a07cad1a777dde10cb72449cfb319744
mohdmaazzaidi <mohdmaazzaidi@gmail.com>
2026-08-01 17:34:27 +0530

    initial commit

---
+++ test.js
@@ -0,0 +1,27 @@
+//CONVERTING STRING INTO NUMBER
+// let a = "23";
+// let ab = Number(a);
+// console.log(typeof ab); 
+// console.log(ab);
+
+
+//CONVERTING NUMBER INTO STRING
+// let a = 233;
+// let ab = String(a);
+// console.log(typeof ab);
+// console.log(ab);
+
+
+//CONVERTING NUMBER INTO BOOLEAN
+// let a = "bfcgh";
+// let ab = Boolean(a);
+// console.log(typeof ab); 
+// console.log(ab);
+
+let a = "2"+(1+5);
+console.log(a);
+
+let b = 3 + 6 + "23";
+
+console.log(b);
+
---
+++ variables.js
@@ -0,0 +1,21 @@
+
+const name = "Maaz";
+let age = 23;
+var course = "B.tech";
+city = "Noida";
+let a = 23;
+let c = 23;
+let c = 34;
+
+
+console.log(a);
+
+// name = "Fahad"; //Cannot change const variable
+
+age = 24;
+
+course = "M.tech";
+
+console.log(typeof city);
+
+console.table([name, age, course, city]);
