cfe95dc3295ee58d08168e1f1f58842e62cc8334
mohdmaazzaidi <mohdmaazzaidi@gmail.com>
2026-08-03 11:53:39 +0530

    Operators_in_JS

---
+++ operators.js
@@ -0,0 +1,65 @@
+// Arithmetic Operators in JS ------------------------
+// +, -, *, /, %, **
+
+// let a = 12+13;   //25
+
+// let b = 12-13;   // -1
+
+// let c = 2*4;   // 8
+
+// let ab = 20/2;  // 10
+
+// let ac = 23%20; // 3
+
+// let ba = 2**5;   // 32
+
+// Comparisin operators in JS -----------------------
+// ==, ===, <=, >=, !=, !==, >, < 
+
+// let a = 3;
+// let b = "3";
+
+// console.log(a==b);  // True
+// console.log(a===b);  // False
+
+// let c = 4;
+// let d = 5;
+// console.log(c<=d);  // True
+// console.log(c>=d);  // False
+// console.log(c>d);  // False
+// console.log(c<d);  // True
+
+
+// let ab = 20;
+// let ba = "20";
+// console.log(ab!=ba);  // False
+// console.log(ab!==ba);  // True
+
+
+// Assignment operator -------------------------------
+
+// let a = 10;
+// console.log(a); // 10
+
+// a+=5;  // a = a+5;
+// console.log(a); // 15
+
+// a-=3; // a = a-3;
+// console.log(a); // 12
+
+// a*=2;  // a = a*2;
+// console.log(a);   // 24
+
+// a/=3;   // a = a/3;
+// console.log(a); // 8
+
+// a%=5;
+// console.log(a); //3
+
+
+
+
+
+
+
+
