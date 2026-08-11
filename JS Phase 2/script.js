bc656a03a9a1fe85e9e4234c7d4e4a31eca38f29
mohdmaazzaidi <mohdmaazzaidi@gmail.com>
2026-08-11 18:41:47 +0530

    Event handling in js

--- JS Phase 2/script.js
+++ JS Phase 2/script.js
@@ -1,2 +1,34 @@
-let ab = document.querySelector("h1");
-ab.innerText = "Bye";
+let p = document.querySelectorAll("p");
+p.forEach(function(val){
+    val.style.fontSize = "22px"
+    let isRed = false;
+    val.addEventListener("click",function(){
+        if(isRed === false){
+            val.style.color = "red";
+            isRed = true;
+        } else {
+            val.style.color = "green";
+            isRed = false;
+        }
+    })
+})
+
+let h1 = document.querySelectorAll("h1");
+h1.forEach(function(val){
+    val.remove();
+})
+
+let div = document.querySelector("div");
+div.addEventListener("dblclick",function(){
+    div.style.color = "orange"
+    let content = div.innerText = "Hello this my first JS event class";
+    div.innerHTML = `<h1>${content}</h1>`
+})
+
+let inputBtn = document.querySelector("input");
+inputBtn.addEventListener("input",function(dtls){
+    if(dtls.data !== null && dtls.data !== " "){
+        console.log(dtls.data);
+    }
+})
+
