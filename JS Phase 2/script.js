c74dc2017f75343adc67b33d37ae4c6ac0175ccb
mohdmaazzaidi <mohdmaazzaidi@gmail.com>
2026-08-13 15:54:09 +0530

    Practice Events

--- JS Phase 2/script.js
+++ JS Phase 2/script.js
@@ -14,10 +14,18 @@ p.forEach(function(val){
 })
 
 let div = document.querySelector("div");
+let isIncleased = false;
 div.addEventListener("dblclick", function(){
+    if(isIncleased == false){
     let newMsg = div.textContent;
     div.innerHTML = `<h1>${newMsg}</h1>`
     div.style.color = "Orange"
+    isIncleased = true;
+    } else {
+        div.innerHTML = div.textContent;
+        div.style.color = "green"
+        isIncleased = false;
+    }
 })
 
 let h1 = document.querySelectorAll("h1");
@@ -30,4 +38,25 @@ newEle.setAttribute("placeholder","Enter you name here");
 newEle.setAttribute("type","text");
 newEle.style.display = "block";
 document.querySelector("p").prepend(newEle)
+newEle.addEventListener("input",function(val){
+    if(val !== null){
+        console.log(val.data);
+    }
+})
+
+
+let li = document.querySelectorAll("li");
+li.forEach(function(val,index){
+    if(index%2 === 1){
+        val.style.color = "red";
+    }
+})
+
+let select = document.querySelector("Select");
+let newH3 = document.createElement("h3");
+document.body.append(newH3)
+
+select.addEventListener("change",function(val){
+    newH3.textContent = `You hava selected ${val.target.value} device`
+})
 
